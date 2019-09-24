a394a3ddb905e00a9918a0fd633a313c924db725
remyli <rapi.remyli@gmail.com>
2019-09-24 11:16:52 +0900

    fix: any消す

--- tests/fizzbuzz.test.ts
+++ tests/fizzbuzz.test.ts
@@ -7,8 +7,9 @@ describe("fizzbuzz", (): void => {
     });
 });
 
-const calcProvider = [[1, "1"], [3, "Fizz"], [5, "Buzz"], [15, "FizzBuzz"]];
-describe.each(calcProvider)("calc(%d)", (argv: any, expected) => {
+type CalcTable = [...[number, string][]];
+const calcProvider : CalcTable = [[1, "1"], [3, "Fizz"], [5, "Buzz"], [15, "FizzBuzz"]];
+describe.each(calcProvider)("calc(%d)", (argv: number, expected: string) => {
     test(`return ${expected}`, () => {
         expect(calc(argv)).toBe(expected);
     });
